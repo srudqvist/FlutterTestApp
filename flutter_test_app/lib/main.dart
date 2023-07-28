d5ecce000e18fefcad51d536d97ecb20eea80fe4
Sam <60146956+srudqvist@users.noreply.github.com>
2023-07-28 10:43:01 -0500

    changed app bar

--- flutter_test_app/lib/main.dart
+++ flutter_test_app/lib/main.dart
@@ -84,7 +84,8 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Settings"),
+        // title: Text(widget.title),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
